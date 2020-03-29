13a239ae48c05261dcd5c55a04527986271114a6
k.hantzen <kevin.hantzen@gmail.com>
2020-03-30 00:05:49 +0200

    4-extract-test-runner-test-into-a-dedicated-test-class
    commit ab4fcb5b413dff10379cf7183f3a143fae3ddcf4
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Mon Mar 30 00:05:14 2020 +0200
    
        Make test method non static
    
    commit 2c5f8f58356a601c6c32ed18f165df00f3f99bca
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Mon Mar 30 00:03:11 2020 +0200
    
        Extract second test method into the ToBeNamed Class
    
    commit e3e4efa978b38016425b00ea88d7eeb1166ff75a
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Mon Mar 30 00:02:08 2020 +0200
    
        Extract first test method into a dedicated class not named yet
    
    commit dfca36d68e4501e61a6446eb61cfb420e6e7c516
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Mon Mar 30 00:00:06 2020 +0200
    
        Extract our two test into two separate method

--- src/test/java/fr/noether/arolla/testception/TestRunner.java
+++ src/test/java/fr/noether/arolla/testception/TestRunner.java
@@ -3,35 +3,9 @@ package fr.noether.arolla.testception;
 public class TestRunner {
 
     public static void main(String[] args) {
-        //---- Test Should be Called ----
-        var testMethodShouldBeCalled = new MyTestClass();
-
-        if (testMethodShouldBeCalled.hasBeenCalled()) {
-            throw new RuntimeException("Test method should have not been called yet.");
-        }
-
-        testMethodShouldBeCalled.run(MyTestClass::thisTestMethodShouldBeCalled);
-
-        if (!testMethodShouldBeCalled.hasBeenCalled()) {
-            throw new RuntimeException("Test method should have been called.");
-        }
-
-        System.out.println("\u001B[32mTest MethodShouldBeCalled : OK");
-
-        //---- Test Should be Setup ----
-        var testMethodShouldBeSetup = new MyTestClass();
-
-        if(testMethodShouldBeSetup.hasBeenSetUp()) {
-            throw new RuntimeException("Test Method should have not been setup yet.");
-        }
-
-        testMethodShouldBeSetup.run(MyTestClass::thisTestMethodShouldBeSetUp);
-
-        if (!testMethodShouldBeSetup.hasBeenSetUp()) {
-            throw new RuntimeException("Test Method should have been setup.");
-        }
-
-        System.out.println("\u001B[32mTest MethodShouldBeSetup : OK");
+        var testRunner = new ToBeNamed();
+        testRunner.testMethodShouldBeRun();
+        testRunner.testMethodShouldBeSetup();
     }
 
 }
---
+++ src/test/java/fr/noether/arolla/testception/ToBeNamed.java
@@ -0,0 +1,35 @@
+package fr.noether.arolla.testception;
+
+public class ToBeNamed {
+    public void testMethodShouldBeRun() {
+        var testMethodShouldBeCalled = new MyTestClass();
+
+        if (testMethodShouldBeCalled.hasBeenCalled()) {
+            throw new RuntimeException("Test method should have not been called yet.");
+        }
+
+        testMethodShouldBeCalled.run(MyTestClass::thisTestMethodShouldBeCalled);
+
+        if (!testMethodShouldBeCalled.hasBeenCalled()) {
+            throw new RuntimeException("Test method should have been called.");
+        }
+
+        System.out.println("\u001B[32mTest MethodShouldBeCalled : OK");
+    }
+
+    public void testMethodShouldBeSetup() {
+        var testMethodShouldBeSetup = new MyTestClass();
+
+        if(testMethodShouldBeSetup.hasBeenSetUp()) {
+            throw new RuntimeException("Test Method should have not been setup yet.");
+        }
+
+        testMethodShouldBeSetup.run(MyTestClass::thisTestMethodShouldBeSetUp);
+
+        if (!testMethodShouldBeSetup.hasBeenSetUp()) {
+            throw new RuntimeException("Test Method should have been setup.");
+        }
+
+        System.out.println("\u001B[32mTest MethodShouldBeSetup : OK");
+    }
+}
