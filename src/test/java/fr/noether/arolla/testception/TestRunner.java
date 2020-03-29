2d26ee6648d2dd91e916d2591627515019ed5f65
k.hantzen <kevin.hantzen@gmail.com>
2020-03-29 23:18:40 +0200

    Throw error on invalid assertion

--- src/test/java/fr/noether/arolla/testception/TestRunner.java
+++ src/test/java/fr/noether/arolla/testception/TestRunner.java
@@ -3,6 +3,9 @@ package fr.noether.arolla.testception;
 public class TestRunner {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        var assertion = 1 == 2;
+        if (!assertion) {
+            throw new RuntimeException("Invalid assertion");
+        }
     }
 }
