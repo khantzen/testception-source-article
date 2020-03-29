16c63235ddc2df7a04c9652c0e032e7d291af1a5
k.hantzen <kevin.hantzen@gmail.com>
2020-03-29 23:33:08 +0200

    Make the test fail breaking the fake

--- src/test/java/fr/noether/arolla/testception/TestRunner.java
+++ src/test/java/fr/noether/arolla/testception/TestRunner.java
@@ -5,10 +5,14 @@ public class TestRunner {
     public static void main(String[] args) {
         var myTest = new MyTestClass();
 
+        if (myTest.hasBeenCalled()) {
+            throw new RuntimeException("Test method should have not been called yet.");
+        }
+
         myTest.thisTestMethodShouldBeCalled();
 
         if (!myTest.hasBeenCalled()) {
-            throw new RuntimeException("Test method should have been called");
+            throw new RuntimeException("Test method should have been called.");
         }
 
         System.out.println("\u001B[32mTest OK");
