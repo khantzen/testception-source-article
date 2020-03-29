9acf52d1bcf4624d058cd7d2eb4bcba51ccb1b1f
k.hantzen <kevin.hantzen@gmail.com>
2020-03-29 23:31:44 +0200

    Test compile but is still red

---
+++ src/test/java/fr/noether/arolla/testception/MyTestClass.java
@@ -0,0 +1,11 @@
+package fr.noether.arolla.testception;
+
+public class MyTestClass {
+    public void thisTestMethodShouldBeCalled() {
+
+    }
+
+    public boolean hasBeenCalled() {
+        return false;
+    }
+}
