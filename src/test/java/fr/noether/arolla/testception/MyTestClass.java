af3d5fb8cb86201d41286db4ee286af7fc6f1929
k.hantzen <kevin.hantzen@gmail.com>
2020-03-29 23:46:50 +0200

    2-A-Test-Method-Should-Be-Setup
    commit 276772c56f4ccc3e96aeb8afa788ecccade0136f
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Sun Mar 29 23:45:16 2020 +0200
    
        Setup the first test
    
    commit 025eeec84e8a0192c3ccbaf606bebb6c3489aebe
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Sun Mar 29 23:44:48 2020 +0200
    
        Make the test pass
    
    commit b865b5b15ab9c2f0471bfb686861ab77c817b7e9
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Sun Mar 29 23:43:32 2020 +0200
    
        Test compile but is still red
    
    commit 43909e52d6e1c9a41f89f24fdbf811b06c64fb7c
    Author: k.hantzen <kevin.hantzen@gmail.com>
    Date:   Sun Mar 29 23:41:45 2020 +0200
    
        Non compiling test

--- src/test/java/fr/noether/arolla/testception/MyTestClass.java
+++ src/test/java/fr/noether/arolla/testception/MyTestClass.java
@@ -2,16 +2,30 @@ package fr.noether.arolla.testception;
 
 public class MyTestClass {
     private boolean hasBeenCalled;
+    private boolean setUp;
 
     public MyTestClass() {
         this.hasBeenCalled = false;
+        this.setUp = false;
+    }
+
+    public void setUp() {
+        this.setUp = true;
     }
 
     public void thisTestMethodShouldBeCalled() {
         this.hasBeenCalled = true;
     }
 
+    public void thisTestMethodShouldBeSetUp() {
+
+    }
+
     public boolean hasBeenCalled() {
         return this.hasBeenCalled;
+    }
+
+    public boolean hasBeenSetUp() {
+        return this.setUp;
     }
 }
--- src/test/java/fr/noether/arolla/testception/TestRunner.java
+++ src/test/java/fr/noether/arolla/testception/TestRunner.java
@@ -3,18 +3,36 @@ package fr.noether.arolla.testception;
 public class TestRunner {
 
     public static void main(String[] args) {
-        var myTest = new MyTestClass();
+        //---- Test Should be Setup
+        var testMethodShouldBeCalled = new MyTestClass();
 
-        if (myTest.hasBeenCalled()) {
+        if (testMethodShouldBeCalled.hasBeenCalled()) {
             throw new RuntimeException("Test method should have not been called yet.");
         }
 
-        myTest.thisTestMethodShouldBeCalled();
+        testMethodShouldBeCalled.setUp();
+        testMethodShouldBeCalled.thisTestMethodShouldBeCalled();
 
-        if (!myTest.hasBeenCalled()) {
+        if (!testMethodShouldBeCalled.hasBeenCalled()) {
             throw new RuntimeException("Test method should have been called.");
         }
 
-        System.out.println("\u001B[32mTest OK");
+        System.out.println("\u001B[32mTest MethodShouldBeCalled : OK");
+
+        //---- Test Should be Setup ----
+        var testMethodShouldBeSetup = new MyTestClass();
+
+        if(testMethodShouldBeSetup.hasBeenSetUp()) {
+            throw new RuntimeException("Test Method should have not been setup yet.");
+        }
+
+        testMethodShouldBeSetup.setUp();
+        testMethodShouldBeSetup.thisTestMethodShouldBeSetUp();
+
+        if (!testMethodShouldBeSetup.hasBeenSetUp()) {
+            throw new RuntimeException("Test Method should have been setup.");
+        }
+
+        System.out.println("\u001B[32mTest MethodShouldBeSetup : OK");
     }
 }
