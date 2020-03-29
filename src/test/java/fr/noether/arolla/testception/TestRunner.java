43909e52d6e1c9a41f89f24fdbf811b06c64fb7c
k.hantzen <kevin.hantzen@gmail.com>
2020-03-29 23:41:45 +0200

    Non compiling test

--- src/test/java/fr/noether/arolla/testception/TestRunner.java
+++ src/test/java/fr/noether/arolla/testception/TestRunner.java
@@ -3,16 +3,33 @@ package fr.noether.arolla.testception;
 public class TestRunner {
 
     public static void main(String[] args) {
-        var myTest = new MyTestClass();
+        //---- Test Should be Setup
+        var testMethodShouldBeCalled = new MyTestClass();
 
-        if (myTest.hasBeenCalled()) {
+        if (testMethodShouldBeCalled.hasBeenCalled()) {
             throw new RuntimeException("Test method should have not been called yet.");
         }
 
-        myTest.thisTestMethodShouldBeCalled();
+        testMethodShouldBeCalled.thisTestMethodShouldBeCalled();
 
-        if (!myTest.hasBeenCalled()) {
+        if (!testMethodShouldBeCalled.hasBeenCalled()) {
             throw new RuntimeException("Test method should have been called.");
+        }
+
+        System.out.println("\u001B[32mTest OK");
+
+        //---- Test Should be Setup ----
+        var testMethodShouldBeSetup = new MyTestClass();
+
+        if(testMethodShouldBeSetup.hasBeenSetUp()) {
+            throw new RuntimeException("Test Method should have not been setup yet.");
+        }
+
+        testMethodShouldBeSetup.setUp();
+        testMethodShouldBeSetup.thisTestMethodShouldBeSetUp();
+
+        if (!testMethodShouldBeSetup.hasBeenSetup()) {
+            throw new RuntimeException("Test Method should have been setup.");
         }
 
         System.out.println("\u001B[32mTest OK");
