ca7a05ac002346d55accee43d3277a8d3f3c7c5a
k.hantzen <kevin.hantzen@gmail.com>
2020-03-30 00:09:22 +0200

    Make ToBeNamed test method run the same as MyTestClass

--- src/test/java/fr/noether/arolla/testception/TestRunner.java
+++ src/test/java/fr/noether/arolla/testception/TestRunner.java
@@ -4,7 +4,8 @@ public class TestRunner {
 
     public static void main(String[] args) {
         var testRunner = new ToBeNamed();
-        testRunner.testMethodShouldBeRun();
+
+        testRunner.run(ToBeNamed::testMethodShouldBeRun);
         testRunner.testMethodShouldBeSetup();
     }
 
