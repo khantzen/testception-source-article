deca0827ea09752c16c734cd65d5cc90fd5f10bb
k.hantzen <kevin.hantzen@gmail.com>
2020-04-05 20:06:51 +0200

    Remove redondant test

--- src/test/java/fr/noether/arolla/testception/TestCaseTest.java
+++ src/test/java/fr/noether/arolla/testception/TestCaseTest.java
@@ -11,38 +11,6 @@ public class TestCaseTest extends TestCase {
 
     public void setUp() {}
 
-    public void testMethodShouldBeRun() {
-        var testMethodShouldBeCalled = new MyTestClass(MyTestClass::thisTestMethodShouldBeCalled);
-
-        if (testMethodShouldBeCalled.hasBeenCalled()) {
-            throw new RuntimeException("Test method should have not been called yet.");
-        }
-
-        testMethodShouldBeCalled.run();
-
-        if (!testMethodShouldBeCalled.hasBeenCalled()) {
-            throw new RuntimeException("Test method should have been called.");
-        }
-
-        System.out.println("\u001B[32mTest MethodShouldBeCalled : OK");
-    }
-
-    public void testMethodShouldBeSetup() {
-        var testMethodShouldBeSetup = new MyTestClass(MyTestClass::thisTestMethodShouldBeSetUp);
-
-        if(testMethodShouldBeSetup.hasBeenSetUp()) {
-            throw new RuntimeException("Test Method should have not been setup yet.");
-        }
-
-        testMethodShouldBeSetup.run();
-
-        if (!testMethodShouldBeSetup.hasBeenSetUp()) {
-            throw new RuntimeException("Test Method should have been setup.");
-        }
-
-        System.out.println("\u001B[32mTest MethodShouldBeSetup : OK");
-    }
-
     public void testMethodShouldBeSetupBeforeRun() {
         var testMethodShouldBeSetupBeforeRun = new MyTestClass(MyTestClass::thisTestMethodShouldBeSetUp);
 
--- src/test/java/fr/noether/arolla/testception/TestRunner.java
+++ src/test/java/fr/noether/arolla/testception/TestRunner.java
@@ -3,8 +3,6 @@ package fr.noether.arolla.testception;
 public class TestRunner {
 
     public static void main(String[] args) {
-        new TestCaseTest(TestCaseTest::testMethodShouldBeRun).run();
-        new TestCaseTest(TestCaseTest::testMethodShouldBeSetup).run();
         new TestCaseTest(TestCaseTest::testMethodShouldBeSetupBeforeRun).run();
     }
 
