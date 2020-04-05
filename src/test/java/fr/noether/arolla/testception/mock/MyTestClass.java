59f5730dfaab141a4a805acd1c53e8890889573b
k.hantzen <kevin.hantzen@gmail.com>
2020-04-05 20:07:41 +0200

    Clean code

--- src/test/java/fr/noether/arolla/testception/mock/MyTestClass.java
+++ src/test/java/fr/noether/arolla/testception/mock/MyTestClass.java
@@ -5,37 +5,20 @@ import fr.noether.arolla.testception.TestCase;
 import java.util.function.Consumer;
 
 public class MyTestClass extends TestCase {
-    private boolean hasBeenCalled;
-    private boolean setUp;
     private String log;
 
     public MyTestClass(Consumer<MyTestClass> testMethod) {
         super(testMethod);
-        this.hasBeenCalled = false;
-        this.setUp = false;
         this.log = "";
     }
 
     @Override
     public void setUp() {
-        this.setUp = true;
         this.log = "setup";
-    }
-
-    public void thisTestMethodShouldBeCalled() {
-        this.hasBeenCalled = true;
     }
 
     public void thisTestMethodShouldBeSetUp() {
         this.log += " run";
-    }
-
-    public boolean hasBeenCalled() {
-        return this.hasBeenCalled;
-    }
-
-    public boolean hasBeenSetUp() {
-        return this.setUp;
     }
 
     public String log() {
